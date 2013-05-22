1f04ff5bf5205bf5d77bd133418b61d6ce8b8bdb
gomezhyuuga <gomezhyuuga@gmail.com>
2013-05-21 19:21:19 -0500

    fix migration

--- db/migrate/20130310210058_change_column_observaciones_to_text_of_inscripcions.rb
+++ db/migrate/20130310210058_change_column_observaciones_to_text_of_inscripcions.rb
@@ -1,7 +1,7 @@
 class ChangeColumnObservacionesToTextOfInscripcions < ActiveRecord::Migration
   def up
   	change_column :inscripcions, :observaciones, :text
-  	add_column :inscripcions, :ncontrol, :string
+  	add_column :inscripcions, :nControl, :string
   end
 
   def down
