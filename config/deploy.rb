8fce48fa08e16157f576d5478bf69fd61d225fc8
gomezhyuuga <gomezhyuuga@gmail.com>
2013-07-16 17:42:44 -0500

    Cambiado host por serviciosocial.uacm.edu.mx

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,11 +13,11 @@ set :use_sudo, false
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "sirass.uacm.edu.mx"                          # Your HTTP server, Apache/etc
-role :app, "sirass.uacm.edu.mx"                          # This may be the same as your `Web` server
-role :db,  "sirass.uacm.edu.mx", :primary => true # This is where Rails migrations will run
+role :web, "serviciosocial.uacm.edu.mx"                          # Your HTTP server, Apache/etc
+role :app, "serviciosocial.uacm.edu.mx"                          # This may be the same as your `Web` server
+role :db,  "serviciosocial.uacm.edu.mx", :primary => true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
-server "sirass.uacm.edu.mx", :app, :web, :db, :primary => true
+server "serviciosocial.uacm.edu.mx", :app, :web, :db, :primary => true
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
